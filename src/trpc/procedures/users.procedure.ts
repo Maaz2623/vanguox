300bb571645d67def2e4e5e791a6b85f7ed60fc5
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-16 19:02:28 +0530

    modified:   src/trpc/procedures/users.procedure.ts

--- src/trpc/procedures/users.procedure.ts
+++ src/trpc/procedures/users.procedure.ts
@@ -1,25 +1,16 @@
-import { db } from "@/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
-import { user } from "@/db/schema";
 import { TRPCError } from "@trpc/server";
-import { eq } from "drizzle-orm";
 
 export const usersRouter = createTRPCRouter({
   getUserData: protectedProcedure.query(async ({ ctx }) => {
-    const { userId } = ctx;
+    const { userData: user } = ctx;
 
-    const [data] = await db
-      .select()
-      .from(user)
-      .where(eq(user.id, userId))
-      .limit(1);
-
-    if (!data) {
+    if (!user) {
       throw new TRPCError({
         code: "NOT_FOUND",
       });
     }
 
-    return data;
+    return user;
   }),
 });
