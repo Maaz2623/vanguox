071646aeb174823bc3e3b913c91d84f160c61fd6
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-19 21:56:04 +0530

    modified:   src/app/(store-front)/stores/[storeName]/layout.tsx 	modified:   src/lib/utils.ts

--- src/app/(store-front)/stores/[storeName]/layout.tsx
+++ src/app/(store-front)/stores/[storeName]/layout.tsx
@@ -1,10 +1,18 @@
+import { headers } from "next/headers";
 import { StoreNavbar } from "./_components/store-navbar";
+import { getSubdomain } from "@/lib/utils";
 
 export default async function StoreLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const host = (await headers()).get("host")!;
+
+  const subdomain = getSubdomain(host);
+
+  if (!subdomain) return <div>No store with this name</div>;  
+
   return (
     <div>
       <StoreNavbar />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getSubdomain(hostname: string): string | null {
+  const parts = hostname.split(".");
+  if (parts.length < 3) return null;
+  return parts[0];
 }
