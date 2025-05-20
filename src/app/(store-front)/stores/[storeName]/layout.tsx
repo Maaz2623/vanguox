77e8bf4ef73678c58c6872e8ceb7fb25f731898c
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-20 21:51:44 +0530

    modified:   src/app/(store-front)/stores/[storeName]/layout.tsx 	modified:   src/app/(vanguox-front)/page.tsx

--- src/app/(store-front)/stores/[storeName]/layout.tsx
+++ src/app/(store-front)/stores/[storeName]/layout.tsx
@@ -1,4 +1,4 @@
-export default async function StoreFrontLayout({
+export default async function SubdomainLayout({
   children,
 }: {
   children: React.ReactNode;
--- src/app/(vanguox-front)/page.tsx
+++ src/app/(vanguox-front)/page.tsx
@@ -1,8 +1,29 @@
+import { getSubdomain } from "@/helpers/get-subdomain";
+import StoreFrontLayout from "@/modules/store-front/store-front-layout";
 import VanguoxFrontLayout from "@/modules/vanguox-front/vanguox-front-layout";
+import { headers } from "next/headers";
 import React from "react";
+import { notFound } from "next/navigation";
 
-const MainPage = () => {
-  return <VanguoxFrontLayout />;
+const MainPage = async () => {
+  if (process.env.NODE_ENV === "production") {
+    const host = (await headers()).get("host") || ""; // ❗ no await
+
+    const subdomain = await getSubdomain(host);
+
+    // Optionally check against a whitelist of valid subdomains
+    if (host && host !== "vanguox.com" && !subdomain) {
+      notFound(); // Show 404 page if subdomain exists but store not found
+    }
+
+    // Show store layout if subdomain is found
+    if (subdomain) {
+      return <StoreFrontLayout />;
+    }
+  } else {
+    // Otherwise, show the root Vanguox layout
+    return <VanguoxFrontLayout />;
+  }
 };
 
 export default MainPage;
