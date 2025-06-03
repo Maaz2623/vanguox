5e05eb8d83d1fb4dd3476dd88e75a29f49c4e4db
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-06-03 11:17:46 +0530

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import { getQueryClient, trpc } from '@/trpc/server'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-import React, { Suspense } from 'react'
+import React from 'react'
 
 const HomePage = () => {
-
-  const queryClient = getQueryClient()
-
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({
-    text: "Mohammed Maaz"
-  }))
 
   return (
     <div>
