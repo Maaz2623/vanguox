3f1e14c355199e1cef9f406b02b15046d3411780
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-06-03 14:15:47 +0530

    modified:   src/lib/auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,2 +1,7 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient()
+import { createAuthClient } from "better-auth/react";
+export const authClient = createAuthClient({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://vanguox.com"
+      : "http://localhost:3000",
+});
