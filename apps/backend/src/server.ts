377e35ce71892ed6d0c589d4665ffbb1439e8337
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-06-21 20:29:50 +0530

    modified:   apps/backend/src/server.ts 	modified:   apps/frontend/src/app/page.tsx

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,25 +1,27 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
 import { serve } from "@hono/node-server";
-import { auth } from "./lib/auth";
 
 const app = new Hono();
 
 // ✅ Auth passthrough
-app.on(
-  ["POST", "GET"],
-  "/api/auth/*",
-  cors({
-    origin: "https://vanguox.com", // replace with your origin
-    allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
-    exposeHeaders: ["Content-Length"],
-    maxAge: 600,
-    credentials: true,
-  }),
-  (c) => {
-    return auth.handler(c.req.raw);
-  }
-);
+// app.on(
+//   ["POST", "GET"],
+//   "/api/auth/*",
+//   cors({
+//     origin: "https://vanguox.com", // replace with your origin
+//     allowHeaders: ["Content-Type", "Authorization"],
+//     allowMethods: ["POST", "GET", "OPTIONS"],
+//     exposeHeaders: ["Content-Length"],
+//     maxAge: 600,
+//     credentials: true,
+//   }),
+//   (c) => {
+//     return auth.handler(c.req.raw);
+//   }
+// );
+
+app.get("/", (c) => {
+  return c.json("Welcome Maaz");
+});
 
 serve({ fetch: app.fetch, port: 5000 });
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,5 +1,22 @@
-const HomePage = async () => {
-  return <div>HomePage </div>;
+"use client";
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  const handleGet = async () => {
+    const res = await fetch("https://https://vanguox.onrender.com/", {
+      method: "GET",
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      HomePage
+      <Button onClick={handleGet}>Get</Button>
+    </div>
+  );
 };
 
 export default HomePage;
