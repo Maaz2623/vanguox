be1c713cb9dc2bbbe111aafa4512afb4308ed68f
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-03-20 12:52:29 +0530

    deleted:    components.json.bak 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/features/auth/components/login-form.tsx

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,9 +1,51 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
+const loginSchema = z.object({
+  email: z.email("Please enter a valid email address"),
+  password: z.string().min(1, "Password is required"),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
 
 export const LoginForm = () => {
   const router = useRouter();
 
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = () => {};
+
+  const isPending = form.formState.isSubmitting;
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome Back</CardTitle>
+          <CardDescription>Login to continue</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
