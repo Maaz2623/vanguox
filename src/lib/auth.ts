703912d4e3cb279322182797d05ace2bce0ca8a1
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-03-27 10:50:29 +0530

    modified:   .gitignore 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   pnpm-workspace.yaml 	new file:   prisma.config.ts 	new file:   prisma/schema.prisma 	modified:   src/lib/auth.ts 	modified:   src/lib/db.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,2 +1,7 @@
 import { betterAuth } from "better-auth";
+import { PrismaClient } from "../../prisma/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+
+const prisma = new PrismaClient(prisma);
 export const auth = betterAuth({});
